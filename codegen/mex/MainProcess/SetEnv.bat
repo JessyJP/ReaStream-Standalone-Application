d85943e55e401ab0227d2b59f9b0219e6f949a21
JessyJP <49342341+JessyJP@users.noreply.github.com>
2022-05-17 18:02:55 +0300

    update

--- codegen/mex/MainProcess/SetEnv.bat
+++
@@ -1,4 +0,0 @@
-set PATH=C:\ProgramData\MATLAB\SupportPackages\R2021a\3P.instrset\mingw_w64.instrset\bin;D:\Program Files\MATLAB\R2021a\extern\include\win64;D:\Program Files\MATLAB\R2021a\extern\include;D:\Program Files\MATLAB\R2021a\simulink\include;D:\Program Files\MATLAB\R2021a\lib\win64;%MATLAB_BIN%;%PATH%
-set INCLUDE=C:\ProgramData\MATLAB\SupportPackages\R2021a\3P.instrset\mingw_w64.instrset\include;;%INCLUDE%
-set LIB=C:\ProgramData\MATLAB\SupportPackages\R2021a\3P.instrset\mingw_w64.instrset\lib;;%LIB%
-set LIBPATH=D:\Program Files\MATLAB\R2021a\extern\lib\win64;%LIBPATH%
